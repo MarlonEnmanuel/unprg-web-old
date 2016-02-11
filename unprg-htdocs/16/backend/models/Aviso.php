1afe98330338ccc8762f13f798daa70858f7f412
Carlos Fernandez <cfernandezmaza@gmail.com>
2016-02-11 17:15:56 -0500

    Agrege campos en Aviso

--- unprg-htdocs/16/backend/models/Aviso.php
+++ unprg-htdocs/16/backend/models/Aviso.php
@@ -4,7 +4,14 @@ require_once "abstractModel.php";
 
 class Aviso extends abstractModel{
 
-	
+	public $fchReg;
+	public $texto;
+	public $emergente;
+	public $visible;
+	public $estado;
+	public $bloqueado;
+	public $idArchivo;
+	public $idUsuario;
 
 	public function __construct(&$mysqli, $id=null){
 		parent::__construct($mysqli, $id);
