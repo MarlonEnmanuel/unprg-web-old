90ebfabc226794aadbc44dca0f91ea4e3fc006e0
Carlos Fernandez <cfernandezmaza@gmail.com>
2016-02-14 02:40:42 -0500

    Terminado el controlAviso y el modelo Aviso

--- unprg-htdocs/16/backend/models/Aviso.php
+++ unprg-htdocs/16/backend/models/Aviso.php
@@ -1,6 +1,7 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'].'/16/backend/config.php';
 require_once config::getRequirePath('backend/models/abstractModel.php');
+require_once 'Archivo.php';
 
 class Aviso extends abstractModel{
 
@@ -106,8 +107,55 @@ class Aviso extends abstractModel{
     }
 
 }
-/*$mysqli = config::getMysqli();
-$user = new Aviso($mysqli, 1);
-print_r($user->searchVisible());
-*/
+
+$mysqli = config::getMysqli();
+$aviso = new Aviso($mysqli, 1);
+
+$lista=array();
+$lista=$aviso->searchVisible();
+$datos='{
+	"avisos" : [';
+for ($i=0; $i < count($lista); $i++) { 
+	$id=$lista[$i]->idAviso;
+	$fchReg=$lista[$i]->fchReg;
+	$texto=$lista[$i]->texto;
+	$emergente=$lista[$i]->emergente;
+	$visible=$lista[$i]->visible;
+	$estado=$lista[$i]->estado;
+	$bloqueado=$lista[$i]->bloqueado;
+	$idArchivo=$lista[$i]->idArchivo;
+	$idUsuario=$lista[$i]->idUsuario;
+	$datos=$datos .'{';
+	$datos=$datos .'"id":"'.$id.'",';
+	$datos=$datos .'"fecha":"'.$fchReg.'",';
+	$archivo= new Archivo($mysqli,$idArchivo);
+	$archivo->get();
+	$resp='false';
+		if($emergente==1){
+			$resp='true';
+		}
+	if($archivo->type=='link'){
+		$datos=$datos.'"img":"'.$archivo->link.'",';
+		$datos=$datos .'"emergente":"'.$resp.'",';
+		$datos=$datos .'"link":"' .$archivo->nombre.'",';
+	}else{
+			$datos=$datos.'"'.$archivo->type.'":"'.$archivo->link.'",' ;
+		
+		$datos=$datos .'"emergente":"'.$resp.'",';
+		$datos=$datos .'"nombre":"'.$archivo->nombre.'",';
+	}
+	
+	$datos=$datos .'"texto:"'.$texto.'"';
+	if($i+1==count($lista)){
+		$datos=$datos .'}';
+	}else{
+		$datos=$datos .'},';
+	}
+	
+
+}
+$datos=$datos .'] }';
+
+print_r($datos);
+
 ?>
