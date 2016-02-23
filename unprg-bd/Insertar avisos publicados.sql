1e2e648ccc85dfc291a776a7236792cd705f018d
Marlon Enmanuel Montalvo Flores <marlon.enmanuel.mf@gmail.com>
2016-02-23 12:57:13 -0500

    Quinta entrega, primera version del sistema (corregido)

--- unprg-bd/Insertar avisos publicados.sql
+++ unprg-bd/Insertar avisos publicados.sql
@@ -21,13 +21,7 @@
 
 LOCK TABLES `archivo` WRITE;
 /*!40000 ALTER TABLE `archivo` DISABLE KEYS */;
-INSERT INTO `archivo` VALUES 
-(1,'Comunicado','img','/frontend/avisos/c4ca4238a0b923820dcc509a6f75849b.jpg','2016-01-26 17:00:00'),
-(2,'Acta 001-2016-VRINV','doc','/frontend/avisos/c81e728d9d4c2f636f067f89cc14862c.pdf','2016-01-28 17:00:00'),
-(3,'http://unprg.edu.pe/resexm/index.html','link','/frontend/avisos/eccbc87e4b5ce2fe28308fd9f2a7baf3.jpg','2016-02-07 17:00:00'),
-(4,'16/documentos/','link','/frontend/avisos/a87ff679a2f3e71d9181a67b7542122c.jpg','2016-02-08 17:00:00'),
-(5,'UNPRG_Invitacion','img','/frontend/avisos/45c48cce2e2d7fbdea1afc51c7c6ad26.jpeg','2016-02-23 17:34:37'),
-(6,'unprg_comunicado','img','/frontend/avisos/d3d9446802a44259755d38e6d163e820.jpeg','2016-02-23 17:37:06');
+INSERT INTO `archivo` VALUES (1,'Comunicado','img','/frontend/avisos/c4ca4238a0b923820dcc509a6f75849b.jpg','2016-01-26 17:00:00'),(2,'Acta 001-2016-VRINV','doc','/frontend/avisos/c81e728d9d4c2f636f067f89cc14862c.pdf','2016-01-28 17:00:00'),(3,'http://unprg.edu.pe/resexm/index.html','link','/frontend/avisos/eccbc87e4b5ce2fe28308fd9f2a7baf3.jpg','2016-02-07 17:00:00'),(4,'16/documentos/','link','/frontend/avisos/a87ff679a2f3e71d9181a67b7542122c.jpg','2016-02-08 17:00:00'),(9,'UNPRG_Invitacion','img','/frontend/avisos/45c48cce2e2d7fbdea1afc51c7c6ad26.jpeg','2016-02-23 17:34:37'),(10,'unprg_comunicado','img','/frontend/avisos/d3d9446802a44259755d38e6d163e820.jpeg','2016-02-23 17:37:06');
 /*!40000 ALTER TABLE `archivo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -37,15 +31,10 @@ UNLOCK TABLES;
 
 LOCK TABLES `aviso` WRITE;
 /*!40000 ALTER TABLE `aviso` DISABLE KEYS */;
-INSERT INTO `aviso` VALUES 
-(1,'2016-01-26 17:00:00','La Oficina General de Recursos Humanos se dirige al personal de trabajadores de la UNPRG para expresar disculpas',0,0,1,1,0,1,1),
-(2,'2016-01-28 17:00:00','Acta N° 001-2016-VRINV Reunión de Trabajo del Vicerrectorado de Investigación',0,0,1,1,0,2,1),
-(3,'2016-02-07 17:00:00','Resultados del Primer Parcial del Centro Pre UNPRG Ciclo 2016-I',1,0,1,1,0,3,1),
-(4,'2016-02-08 17:00:00','El Vicerrectorado de Investigación, en su proceso de implementación, pone a su disposición los siguientes documentos',0,1,1,1,0,4,1),
-(5,'2016-02-23 17:34:37','CEREMONIA DE ASUNCIÓN DEL RECTOR DE LA UNPRG (Periodo 2016-2020)',0,1,1,1,0,5,1),
-(6,'2016-02-23 17:37:06','El Rector, ViceRectores, Decanos y funcionarios de la nueva administración de la UNPRG, con el pleno respaldo que la ley otorga y en primacía del principio de autoridad, hace de conocimiento a la comunidad universitaria.',0,1,1,1,0,6,1);
+INSERT INTO `aviso` VALUES (1,'2016-01-26 17:00:00','La Oficina General de Recursos Humanos se dirige al personal de trabajadores de la UNPRG para expresar disculpas',0,0,1,1,0,1,1),(2,'2016-01-28 17:00:00','Acta N° 001-2016-VRINV Reunión de Trabajo del Vicerrectorado de Investigación',0,0,1,1,0,2,1),(3,'2016-02-07 17:00:00','Resultados del Primer Parcial del Centro Pre UNPRG Ciclo 2016-I',1,0,1,1,0,3,1),(4,'2016-02-08 17:00:00','El Vicerrectorado de Investigación, en su proceso de implementación, pone a su disposición los siguientes documentos',0,1,1,1,0,4,1),(9,'2016-02-23 17:34:37','CEREMONIA DE ASUNCIÓN DEL RECTOR DE LA UNPRG (Periodo 2016-2020)',0,1,1,1,0,9,1),(10,'2016-02-23 17:37:06','El Rector, ViceRectores, Decanos y funcionarios de la nueva administración de la UNPRG, con el pleno respaldo que la ley otorga y en primacía del principio de autoridad, hace de conocimiento a la comunidad universitaria.',0,1,1,1,0,10,1);
 /*!40000 ALTER TABLE `aviso` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -55,4 +44,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-02-23 12:43:28
+-- Dump completed on 2016-02-23 12:56:18
